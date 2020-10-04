72abb07de9758c54c6d259f7564e6256ad56637a
Jake <jaka.presecnik@gmail.com>
2020-10-04 14:12:09 +0200

    Updates

---
+++ precache-manifest.7157d5023c0395b9cb1561361f08c09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa76b491797cd7b5ec1a6b0baf129a8b",
+    "url": "/JakaPresecnik/would-you-rather/index.html"
+  },
+  {
+    "revision": "62b3ecab689652a0d8e5",
+    "url": "/JakaPresecnik/would-you-rather/static/css/main.07244324.chunk.css"
+  },
+  {
+    "revision": "2f78fe9fab166eea92af",
+    "url": "/JakaPresecnik/would-you-rather/static/js/2.4c451430.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JakaPresecnik/would-you-rather/static/js/2.4c451430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b3ecab689652a0d8e5",
+    "url": "/JakaPresecnik/would-you-rather/static/js/main.dd0df160.chunk.js"
+  },
+  {
+    "revision": "d91671e7915223dc93c5",
+    "url": "/JakaPresecnik/would-you-rather/static/js/runtime-main.9e26e0f8.js"
+  }
+]);
